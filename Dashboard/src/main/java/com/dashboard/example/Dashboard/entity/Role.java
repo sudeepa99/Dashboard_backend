4fece2814922b490117fdf6078e14eb0a8954e81
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-10-06 19:21:37 +0530

    Role entity created

--- Dashboard/src/main/java/com/dashboard/example/Dashboard/entity/Role.java
+++ Dashboard/src/main/java/com/dashboard/example/Dashboard/entity/Role.java
@@ -6,19 +6,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
-
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Table(name = "User")
-public class User {
+@Entity
+@Table (name = "Role")
+public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int user_id;
+    private int role_id;
     private String name;
-    private String password;
-    private String email;
     private Date created_at;
     private Date updated_at;
+
 }
