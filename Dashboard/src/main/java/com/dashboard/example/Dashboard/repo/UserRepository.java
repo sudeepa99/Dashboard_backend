b77b8553cf50d1abc04990da2fecc03b7a1a4fc1
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-10-08 17:29:51 +0530

    User service and repository updated for user registartion

--- Dashboard/src/main/java/com/dashboard/example/Dashboard/repo/UserRepository.java
+++ Dashboard/src/main/java/com/dashboard/example/Dashboard/repo/UserRepository.java
@@ -3,6 +3,9 @@ package com.dashboard.example.Dashboard.repo;
 import com.dashboard.example.Dashboard.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
--- Dashboard/src/main/java/com/dashboard/example/Dashboard/service/UserService.java
+++ Dashboard/src/main/java/com/dashboard/example/Dashboard/service/UserService.java
@@ -1,12 +1,26 @@
 package com.dashboard.example.Dashboard.service;
 
 import com.dashboard.example.Dashboard.dto.SignupRequestDTO;
+import com.dashboard.example.Dashboard.entity.User;
+import com.dashboard.example.Dashboard.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
 
     public void registerNewUser(SignupRequestDTO signupRequest) {
+        if (userRepository.existsByEmail(signupRequest.getEmail())){
+            throw new RuntimeException("This email already exists");
+        }
+
+        User newUser = new User();
+        newUser.setEmail(signupRequest.getEmail());
+        newUser.setPassword(signupRequest.getPassword());
+
+        userRepository.save(newUser);
 
     }
 }
