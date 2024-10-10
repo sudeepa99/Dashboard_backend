9b210239735aac3bd0dbf8faeca1189ed4c1043e
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-10-10 23:22:05 +0530

    Auth servivces updated

--- Dashboard/src/main/java/com/dashboard/example/Dashboard/service/AuthService.java
+++ Dashboard/src/main/java/com/dashboard/example/Dashboard/service/AuthService.java
@@ -3,7 +3,10 @@ package com.dashboard.example.Dashboard.service;
 import com.dashboard.example.Dashboard.dto.SignupRequestDTO;
 import com.dashboard.example.Dashboard.entity.User;
 import com.dashboard.example.Dashboard.repo.UserRepository;
+import com.dashboard.example.Dashboard.utill.JwtToken;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,24 +16,36 @@ public class AuthService {
     private UserRepository userRepository;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private JwtToken tokenUtil;
 
-    @Autowired
-    private JwtTokenProvider jwtTokenProvider;
-
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
 
-    public void registerNewUser(SignupRequestDTO signupRequest) {
+    @Transactional
+    public User  registerNewUser(SignupRequestDTO signupRequest) {
         if (userRepository.existsByEmail(signupRequest.getEmail())){
             throw new RuntimeException("This email already exists");
         }
-
         User newUser = new User();
         newUser.setName(signupRequest.getName());
         newUser.setEmail(signupRequest.getEmail());
-        newUser.setPassword(signupRequest.getPassword());
+        newUser.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
 
-        userRepository.save(newUser);
+        return userRepository.save(newUser);
 
+    }
+
+    public String login(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(password, user.getPassword())){
+            throw new RuntimeException("Incorrect password");
+        }
+
+        String accessToken = tokenUtil.createAccessToken(user.getUser_id());
+        String refreshToken = tokenUtil.createRefreshToken(user.getUser_id());
+
+        return accessToken + " " + refreshToken;
     }
 }
